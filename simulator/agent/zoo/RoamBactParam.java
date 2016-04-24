426af285d121e7d37aac76a2ba0574319f2cf4c3
MaorG <maor@slatescience.com>
2016-04-24 10:49:48 +0300

    Roaming Bacteria - detach at birth

--- simulator/agent/zoo/RoamBactParam.java
+++ simulator/agent/zoo/RoamBactParam.java
@@ -1,10 +1,14 @@
 package simulator.agent.zoo;
 
 import simulator.Simulator;
+import utils.UnitConverter;
 import utils.XMLParser;
 
 public class RoamBactParam extends BacteriumParam {
 
+	public double xMax;
+	public double yMax;
+	public double zMax;
 	
 	/**
 	 * \brief Creates a parameter storage object for the Bacterium EPS species type
@@ -13,6 +17,29 @@ public class RoamBactParam extends BacteriumParam {
 	 */
 	public RoamBactParam() {
 		super();
+	}
+	
+	/**
+	 * \brief Initialises Bacterium species parameters, calling the relevant superclasses to initialise common parameters for Bacterium derived species
+	 * 
+	 * Initialises Bacterium species parameters, calling the relevant superclasses to initialise common parameters for Bacterium derived species
+	 * 
+	 * @param aSim	The simulation object used to simulate the conditions specified in the protocol file
+	 * @param aSpeciesRoot	A species mark-up within the specified protocol file
+	 */
+	@Override
+	public void init(Simulator aSim, XMLParser aSpeciesRoot, XMLParser speciesDefaults)
+	{
+		super.init(aSim,aSpeciesRoot,speciesDefaults);
+		Double value;
+		
+		value = getSpeciesParameterDouble("roamX", aSpeciesRoot, speciesDefaults);
+		xMax = (value == XMLParser.nullDbl) ? xMax : value;
+		value = getSpeciesParameterDouble("roamX", aSpeciesRoot, speciesDefaults);
+		yMax = (value == XMLParser.nullDbl) ? yMax : value;
+		value = getSpeciesParameterDouble("roamX", aSpeciesRoot, speciesDefaults);
+		zMax = (value == XMLParser.nullDbl) ? zMax : value;
+			
 	}
 	
 	
--- simulator/agent/zoo/RoamingBacterium.java
+++ simulator/agent/zoo/RoamingBacterium.java
@@ -1,5 +1,11 @@
 package simulator.agent.zoo;
 
+import java.math.BigInteger;
+
+import simulator.Simulator;
+import simulator.geometry.ContinuousVector;
+import simulator.World;
+
 public class RoamingBacterium extends Bacterium 
 {
 	/**
@@ -20,6 +26,41 @@ public class RoamingBacterium extends Bacterium
 	}
 	
 	/**
+	 * \brief Create a new Bacterium agent (who a priori is registered in at
+	 * least one container).
+	 * 
+	 * This agent is located on the relevant grid.
+	 */
+	@Override
+	public void createNewAgent(ContinuousVector position) 
+	{
+		
+		if (Math.random() < 0.05) {
+			_Roaming = true;
+		} else {
+			_Roaming = false;
+		}
+			
+		super.createNewAgent(position);
+			
+	}
+	
+	/**
+	 * \brief Initialises any new agent (progenitor or daughter cell), setting
+	 * cell radius, generation, and genealogy.
+	 */
+	@Override
+	public void init() 
+	{
+		if (Math.random() < 0.05) {
+			_Roaming = true;
+		} else {
+			_Roaming = false;
+		}
+		super.init();
+	}
+	
+	/**
 	 * \brief Called at each time step of the simulation to compute mass
 	 * growth and update radius, mass, and volume.
 	 * 
@@ -30,6 +71,21 @@ public class RoamingBacterium extends Bacterium
 	@Override
 	protected void internalStep()
 	{
+		
+		if (_Roaming) {
+
+			double length_X = _species.domain.length_X;
+			double length_Y = _species.domain.length_Y;
+			double length_Z = _species.domain.length_Z;
+			
+			double nextX = length_X * (0.0 + 1.0 * Math.random());
+			double nextY = length_Y * (0.0 + 1.0 * Math.random());
+			double nextZ = length_Z * (0.0 + 1.0 * Math.random());
+	
+			ContinuousVector cc = new ContinuousVector(nextX, nextY, nextZ);		
+			this.setLocation(cc);
+			_agentGrid.registerMove(this);
+		}
 		super.internalStep();
 	}
 }
